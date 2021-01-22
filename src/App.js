58997c455ad880706682b9b83f38cbd82732247b
Shannon Beach <shannonbeach1982@gmail.com>
2021-01-21 19:20:11 -0500

    updated layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import {React, Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const movies = [
+  {
+    id: 1,
+    title: 'Star Wars',
+    desc: 'Space movie'
+  },
+  {
+    id: 2,
+    title: 'Willow'
+  },
+  {
+    id: 3,
+    title: 'Moana'
+  },
+  {
+    id: 4,
+    title: 'Frozen'
+  },
+  {
+    id: 5,
+    title: 'Sing'
+  },
+]
+class App extends Component{
+  state = {
+    movies: []
+  }
+
+  async componentDidMount(){
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=7fc4be7e5a0582ab7a8a43e3e9b966fd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movies: movies.results
+      });
+    } catch(e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {this.state.movies.map((movie)=>
+          <Movie key={movie.id} movie={movie} />
+        )}
+
+      </div>
+    );
+  }
 }
 
 export default App;
