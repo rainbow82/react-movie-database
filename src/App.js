428eb608bbeb28bffab11a1902dcaf75c890e210
Shannon Beach <shannonbeach1982@gmail.com>
2021-01-21 19:39:01 -0500

    covert movie to functional stateless componet from class component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import {React, Component} from 'react';
+/* eslint-disable */
+import { React, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Movie from './Movie'
+import Movie from './Movie';
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star Wars',
-    desc: 'Space movie'
-  },
-  {
-    id: 2,
-    title: 'Willow'
-  },
-  {
-    id: 3,
-    title: 'Moana'
-  },
-  {
-    id: 4,
-    title: 'Frozen'
-  },
-  {
-    id: 5,
-    title: 'Sing'
-  },
-]
-class App extends Component{
+class App extends Component {
   state = {
-    movies: []
+    movies: [],
   }
 
-  async componentDidMount(){
+  async componentDidMount() {
     try {
       const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=7fc4be7e5a0582ab7a8a43e3e9b966fd&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
       console.log(movies);
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch(e) {
       console.log(e);
--- src/Movie.js
+++ src/Movie.js
@@ -1,26 +1,17 @@
-import {React, Component} from 'react';
-import PropTypes from "prop-types";
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class Movie extends Component {
-  // every prop should have a proptype
-  // have an isRequired or a default prop
-  static propTypes = {
-    movie: PropTypes.shape({
-      title: PropTypes.string.isRequired,
-    })
-  }
-
-  // static defaultProps = {
-  //   desc: 'Movie description not available'
-  // }
-
-  render(){
-    return(
-      <div>
-          <h3>{this.props.movie.title}</h3>
-      </div>
-    )
-  }
-}
+const Movie = (props) => (
+  <div>
+    <h3>{props.movie.title}</h3>
+  </div>
+);
 
 export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
